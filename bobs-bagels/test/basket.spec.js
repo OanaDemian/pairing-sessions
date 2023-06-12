1d0eff4603f046cb7a7358fb4a0a41e2c93c97c1
kenichi beveridge <kenbev84@gmail.com>
2023-06-12 17:00:35 +0100

    Added Test for removing a bagel from the basket

--- bobs-bagels/test/basket.spec.js
+++ bobs-bagels/test/basket.spec.js
@@ -4,19 +4,19 @@ const Bagel = require('../src/Bagel');
 const { assertEquals, assertNaN } = require('./testingFramework.js')
 
 let input = new Bagel('Big Bagel');
-let expected, actual, result; // Initially undefined
+let expected, actual, result, basket; // Initially undefined
 
-// Test 1 - [Bagel object with Big Bagel name will be added to the array of bagels. Length of array is shorter by 1
+// Test 1 - [Bagel object with Big Bagel name will be added to the array of bagels. Length of array is longer by 1
 
 console.log(`Test 1`);
 console.log(`==================================================`);
 console.log(``);
 
-// Length of array is shorter by 1
+// Length of array is longer by 1
 expected = 1;
 
 // Act
-let basket = new Basket();
+basket = new Basket();
 basket.addBagel(input);
 actual = basket.basketBagels.length;
 console.log("🚀 ~ file: basket.spec.js:18 ~ actual:", actual);
@@ -29,5 +29,31 @@ result = assertEquals(actual, expected);
 console.log(`Test 1 - {} gives count of 1: ${result ? `PASS` : `FAIL`}`);
 
 // Clean up
-input, expected, actual, result = undefined; // Initially undefined
+input, expected, actual, result, basket = undefined; // Initially undefined
 
+
+// Test 2 - [Bagel object with Big Bagel name will be added to the array of bagels. Length of array is shorter by 1
+
+console.log(`Test 2`);
+console.log(`==================================================`);
+console.log(``);
+
+// ARRANGE
+expected = 0;
+const bagelToAdd = new Bagel('Peanut Butter & Jelly');
+const bagelToRemove = bagelToAdd;
+
+// ACT
+basket = new Basket();
+basket.addBagel(bagelToAdd);
+basket.removeBagel(bagelToRemove);
+actual = basket.basketBagels.length;
+
+// ASSERT
+result = assertEquals(actual, expected);
+
+// Report
+console.log(`Test 2 - {} gives count of 0: ${result ? `PASS` : `FAIL`}`);
+
+// Clean up
+input, expected, actual, result, basket = undefined; // Initially undefined

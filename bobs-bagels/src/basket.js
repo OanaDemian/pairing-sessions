1fa040f10c670043eb62b6027c141ad5eddb597b
kenichi beveridge <kenbev84@gmail.com>
2023-06-12 17:01:32 +0100

    Added test to remove bagel from basket

--- bobs-bagels/src/basket.js
+++ bobs-bagels/src/basket.js
@@ -1,10 +1,14 @@
 
 class Basket {
   constructor() {
-  this.basketBagels = [];
+    this.basketBagels = [];
   }
   addBagel(bagel) {
     this.basketBagels = [...this.basketBagels, bagel];
+  }
+  removeBagel(bagelName) {
+    const index = this.basketBagels.indexOf(bagelName);
+    this.basketBagels.splice(index, 1);
   }
 }
 module.exports = Basket;
